1cab227646daa6a8cf76fb3608f3a1d3663eebf4
HeegyeongJ <144974339+HeegyeongJ@users.noreply.github.com>
2023-10-12 18:14:22 +0900

    player 턴 넘김 구현 (점수로직 오류)

--- assets/app.js
+++ assets/app.js
@@ -1,25 +1,41 @@
 const btnRollDice = document.querySelector('.btn__rolling');
 const btnHold = document.querySelector('.btn__hold');
 const diceImg = document.querySelector('.dice');
-const player1Current = document.querySelector('.player1-current');
-const player2Current = document.querySelector('.player2-current');
+const playerBox = document.querySelectorAll('.player-box');
+const currentPlayer = document.querySelectorAll('.current-score');
+const playerScore = document.querySelectorAll('.player-score');
+let playingPlayerScore;
+let current;
+
+
 
 let rolledNum = 0;
 let currentNum = 0;
+let playerNum = 0;
+
 
 const toggleClass = () => {
-    player1Current.classList.toggle('current-player');
-    player2Current.classList.toggle('current-player'); 
+    playerNum = 0;
+    currentNum = 0
+    current.innerText = 0;
+    for (let i = 0; i < currentPlayer.length; i++){
+        currentPlayer[i].classList.toggle('current-player');
+    }
+    for (let i = 0; i < playerBox.length; i++){
+        playerBox[i].classList.toggle('current-player-bg');
+    }
+    for (let i = 0; i < playerScore.length; i++){
+        playerScore[i].classList.toggle('player-score-plus');
+    }
     
 }
 
 const rollingDice = () => {
     rolledNum = parseInt(Math.random()* 6 + 1);
     diceImg.src = `assets/imgs/dice0${rolledNum}.png`
-    
-    const current = document.querySelector('.current-player');
 
-    
+    current = document.querySelector('.current-player');
+
     if (rolledNum >= 3) {
         currentNum += rolledNum;
         current.innerText = currentNum;
@@ -28,16 +44,21 @@ const rollingDice = () => {
         currentNum = 0;
         current.innerText = 0;
         toggleClass();
-        return;
     }
-    
+
 }
 
-const holding = () => {
 
+const holding = () => {
+    currentNum = current.innerText * 1;
+    playerNum += currentNum;
+    playingPlayerScore = document.querySelector('.player-score-plus');
+    playingPlayerScore.innerText = playerNum;
+    
+    toggleClass();
 }
 
     
 
 btnRollDice.addEventListener('click', rollingDice);
-btnHold.addEventListener('click', holding)
+btnHold.addEventListener('click', holding);
