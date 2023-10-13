cc79b33055bb8ed40d5dadd021471156fdf89adb
HeegyeongJ <144974339+HeegyeongJ@users.noreply.github.com>
2023-10-13 18:34:31 +0900

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,73 +1,110 @@
-const btnRollDice = document.querySelector('.btn__rolling');
-const btnHold = document.querySelector('.btn__hold');
-const diceImg = document.querySelector('.dice');
-const playerBox = document.querySelectorAll('.player-box');
-const currentPlayer = document.querySelectorAll('.current-score');
-const playerScore = document.querySelectorAll('.player-score');
-const body = document.querySelector('body');
-let playingPlayerScore;
+
+
 let current;
 
 
 
 let rolledNum = 0;
 let currentNum = 0;
-let playerNum = 0;
+let player1Count = 0;
+let player2Count = 0;
 
 
 
-const toggleClass = () => {
-    playerNum = 0;
-    currentNum = 0
-    current.innerText = 0;
-    for (let i = 0; i < currentPlayer.length; i++){
-        currentPlayer[i].classList.toggle('current-player');
+function appStart() {
+    const btnRollDice = document.querySelector('.btn__rolling');
+    const btnHold = document.querySelector('.btn__hold');
+    const btnNew = document.querySelector('.btn__new-game');
+
+    const playerBox = document.querySelectorAll('.player-box');
+    const currentPlayer = document.querySelectorAll('.current-score');
+    const playerScore = document.querySelectorAll('.player-score');
+
+
+
+
+    const displayGameover = () => {
+        const body = document.querySelector('body');
+        const div = document.createElement('div');
+        const gameoverDiv = body.appendChild(div);
+        gameoverDiv.classList.add('.gameover');
+        div.innerText = 'gameover'
     }
-    for (let i = 0; i < playerBox.length; i++){
-        playerBox[i].classList.toggle('current-player-bg');
+    const gameOver = () => {
+        btnRollDice.removeEventListener('click', rollingDice);
+        btnHold.removeEventListener('click', holding);
+        displayGameover();
     }
-    for (let i = 0; i < playerScore.length; i++){
-        playerScore[i].classList.toggle('player-score-plus');
-    }
-    
-}
 
-const rollingDice = () => {
-    rolledNum = parseInt(Math.random()* 6 + 1);
-    diceImg.src = `assets/imgs/dice0${rolledNum}.png`
-
-    current = document.querySelector('.current-player');
-
-    if (rolledNum >= 3) {
-        currentNum += rolledNum;
-        current.innerText = currentNum;
-    } else {
-        rolledNum = 0;
-        currentNum = 0;
+    const toggleClass = () => {
+        currentNum = 0
         current.innerText = 0;
+        for (let i = 0; i < currentPlayer.length; i++) {
+            currentPlayer[i].classList.toggle('current-player');
+        }
+        for (let i = 0; i < playerBox.length; i++) {
+            playerBox[i].classList.toggle('current-player-bg');
+        }
+        for (let i = 0; i < playerScore.length; i++) {
+            playerScore[i].classList.toggle('player-score-plus');
+        }
+
+    }
+
+    const setNewGame = () => {
+        player1Count = 0;
+        player2Count = 0;
+        for (let i = 0; i < playerScore.length; i++) {
+            playerScore[i].innerText = 0;
+        }
+    }
+
+    const rollingDice = () => {
+        rolledNum = parseInt(Math.random() * 6 + 1);
+        const diceImg = document.querySelector('.dice');
+        diceImg.src = `assets/imgs/dice0${rolledNum}.png`
+
+        current = document.querySelector('.current-player');
+
+        if (rolledNum >= 3) {
+            currentNum += rolledNum;
+            current.innerText = currentNum;
+        } else {
+            rolledNum = 0;
+            currentNum = 0;
+            current.innerText = 0;
+            toggleClass();
+        }
+
+    }
+
+    const holding = () => {
+        currentNum = current.innerText * 1;
+        const playingPlayerScore = document.querySelector('.player-score-plus');
+
+        if (player1Count >= 50 || player2Count >= 50) {
+            gameOver();
+            return;
+        } else {
+            if (playingPlayerScore.dataset.player == 1) {
+                player1Count += currentNum;
+                playingPlayerScore.innerText = player1Count;
+            } else {
+                player2Count += currentNum;
+                playingPlayerScore.innerText = player2Count;
+            }
+        }
+
+
         toggleClass();
     }
 
+
+
+
+    btnRollDice.addEventListener('click', rollingDice);
+    btnHold.addEventListener('click', holding);
+    btnNew.addEventListener('click', setNewGame);
 }
 
-
-const holding = () => {
-    currentNum = current.innerText * 1;
-    playerNum += currentNum;
-    playingPlayerScore = document.querySelector('.player-score-plus');
-  
-    if(playingPlayerScore.innerText*1 == 0){
-        playingPlayerScore.innerText = playerNum;
-    }else if(playingPlayerScore.innerText*1 > 0){
-        playingPlayerScore.innerText = playingPlayerScore.innerText*1 + playerNum;
-    }else if (playingPlayerScore.innerText*1 >= 50){
-        
-    }
-    
-    toggleClass();
-}
-
-    
-
-btnRollDice.addEventListener('click', rollingDice);
-btnHold.addEventListener('click', holding);
+appStart();
